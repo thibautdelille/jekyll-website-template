0987d4a6ad86a20f907a24ebd3fc39a8956fd771
Thibaut Delille <tdelille@gmail.com>
2013-12-07 14:15:28 -0800

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,257 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    // load our main definition package
+    pkg: grunt.file.readJSON('package.json'),
+    site: grunt.file.readYAML('_config.yml'),
+
+    /*
+    * url: https://github.com/ai/autoprefixer
+    * description: Autoprefixer uses the data on current browser popularity and properties support to apply prefixes for you:
+      a { transition: transform 1s }
+      become
+      a {
+        -webkit-transition: -webkit-transform 1s;
+        transition: -ms-transform 1s;
+        transition: transform 1s
+      }
+    */
+    autoprefixer: {
+      build: {
+        options: {
+          browsers: ['last 2 versions', '> 1%']
+        },
+        files: [
+          {
+            src : ['**/*.sass.css'],
+            ext : '.autoprefixed.css',
+            expand : true
+          }
+        ]
+      }
+    },
+
+    /*
+    * url: https://github.com/gruntjs/grunt-contrib-concat
+    * description: concat all js plugin into one js file and concat the css prefixed with the icon and font css
+    */
+    concat: {
+      options: {
+        separator: ';',
+        stripBanners: true,
+        banner: '/*!\n<%= pkg.name %>\nv<%= site.version %>\n<%= grunt.template.today("mm-dd-yyyy") %>\nMade at <%= pkg.author.name %> - <%= pkg.author.url %>\n*/'
+      },
+      js: {
+        src: ['assets/vendors/modernizr/modernizr-custom.js', 'assets/js/tgr-*.js'],
+        dest: 'assets/js/thegoodrush.js'
+      },
+      css: {
+        options: {
+          separator: '',
+        },
+        src: ['assets/css/style.autoprefixed.css' ],
+        dest: 'assets/css/style.css'
+      }
+    },
+
+    /*
+    * url: https://github.com/gruntjs/grunt-contrib-connect
+    * description: start a server on port 8000 for the documentation
+    */
+    connect: {
+      server: {
+        options: {
+          hostname: '*',
+          port: 3105,
+          base: 'dist/'
+        }
+      }
+    },
+
+    /*
+    * url: https://github.com/gruntjs/grunt-contrib-copy
+    * description: copy icon, js and vendors into the documentation
+    */
+    copy: {
+      // Copy vendors to Jekyll folder
+      vendors: {
+        files: [
+          { 
+            expand: true, 
+            cwd: './assets/vendors', 
+            src: ['./**/*.*', '!./modernizr/'], 
+            dest: 'jekyll/assets/vendors/' 
+          }
+        ]
+      },
+      // Copy img to Jekyll folder
+      images: {
+        files: [
+          { 
+            expand: true, 
+            cwd: './assets/img', 
+            src: ['./**/*.*'], 
+            dest: 'jekyll/assets/vendors/' 
+          }
+        ]
+      }
+    },
+
+    /*
+    * url: https://github.com/gruntjs/grunt-contrib-cssmin
+    * description: minify the css
+    */
+
+    cssmin: {
+      minify: {
+        expand: true,
+        cwd: 'assets/css/',
+        src: ['*.css', '!*.min.css', '!*.sass.css', '!*.autoprefixed.css'],
+        dest: 'jekyll/assets/css/',
+        ext: '.min.css'
+      }
+    },
+
+    /*
+    * url: https://github.com/dannygarcia/grunt-jekyll
+    * description: Run Jekyll
+    */
+    jekyll: {
+      options: {
+        src: './jekyll'
+      },
+      dev: {
+        options: {
+          dest: './dist',
+          config: '_config.yml'
+        }
+      }
+    },
+
+    /*
+    * url: https://github.com/gruntjs/grunt-contrib-jshint
+    * description: Validate files with JSHint.
+    */
+    jshint: {
+      all: [
+        'assets/js/**/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    /*
+    * url: https://github.com/Modernizr/grunt-modernizr
+    * description: sifts through your project files, gathers up your references to Modernizr tests and outputs a lean, mean Modernizr machine.
+    */
+    modernizr: {
+      "devFile" : "assets/vendors/modernizr/modernizr-dev.js",
+      "outputFile" : "assets/vendors/modernizr/modernizr.js",
+      "extra" : {
+        "shiv" : true,
+        "printshiv" : false,
+        "load" : true,
+        "mq" : true,
+        "cssclasses" : true
+      },
+      "extensibility" : {
+        "addtest" : false,
+        "prefixed" : true,
+        "teststyles" : false,
+        "testprops" : false,
+        "testallprops" : false,
+        "hasevents" : false,
+        "prefixes" : false,
+        "domprefixes" : false
+      },
+      "uglify" : true,
+      "tests" : ["csstransitions"],
+      "parseFiles" : false,
+      "matchCommunityTests" : false,
+      "customTests" : []
+    },
+
+    /*
+    * https://github.com/gruntjs/grunt-contrib-sass
+    * description: compile sass to css
+    */
+    sass: {
+      build: {
+        files : [
+          {
+            src : ['style.scss'],
+            cwd : 'assets/scss',
+            dest : 'assets/css/',
+            ext : '.sass.css',
+            expand : true
+          }
+        ],
+        options : {
+          style : 'expanded'
+        }
+      }
+    },
+
+    /*
+    * url: https://github.com/gruntjs/grunt-contrib-uglify
+    * description: Minify files with UglifyJS.
+    */
+    uglify: {
+      options: {
+        report: true
+      },
+      js: {
+        files: {
+          'jekyll/assets/js/thegoodrush.min.js': ['assets/js/thegoodrush.js']
+        }
+      }
+    },
+
+    /*
+    * url: https://github.com/gruntjs/grunt-contrib-watch
+    * description: Run predefined tasks whenever watched file patterns are added, changed or deleted.
+    */
+    watch: {
+      scss: {
+        files: ['assets/scss/**/*.scss'],
+        tasks: ['scss']
+      },
+      js: {
+        files: ['assets/js/*.js'],
+        tasks: ['js']
+      },
+      jekyll: {
+        files: ['jekyll/**/*.markdown', 'jekyll/**/*.md',  'jekyll/**/*.html'],
+        tasks: 'jekyll'
+      },
+      livereload: {
+        options: {
+          livereload: true
+        },
+        files: [
+          'dist/**/*.html',
+          'dist/assets/css/{,*/}*.css',
+          'dist/assets/js/{,*/}*.js'
+        ]
+      }
+    }
+
+  });
+
+
+  grunt.registerTask('scss', ['sass', 'autoprefixer', 'concat:css', 'cssmin']);
+  grunt.registerTask('js', ['jshint', 'modernizr', 'concat:js', 'uglify:js']);
+  grunt.registerTask('vendors', ['copy:vendors']);
+  grunt.registerTask('images', ['copy:images']);
+
+  grunt.registerTask('default', ['scss','js','images','vendors', 'jekyll']);
+
+  grunt.registerTask('watch', ['connect:server', 'watch']);
+
+  grunt.loadNpmTasks('grunt-jekyll');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-modernizr');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  require('matchdep').filterDev('grunt-contrib*').forEach(grunt.loadNpmTasks);
+};
---
+++ README.md
@@ -0,0 +1,17 @@
+# The Good Rush
+---
+
+### Install the Project
+ 1. Run `npm install` (it use [Node](http://nodejs.org/) and [npm](https://npmjs.org/))
+ 2. If necessary, [install Jekyll](http://jekyllrb.com/docs/installation/) (requires v1.x).
+ 3. Run `grunt`, Make sure you have the Grunt command-line interface with `npm install -g grunt-cli`
+ 4. Run `grunt watch`
+ 5. Open [http://localhost:3105](http://localhost:3105) in your browser, and voilà.
+
+This Project use
+- [Sass](http://sass-lang.com/)
+- [Node](http://nodejs.org/) and [npm](https://npmjs.org/) so we can...
+- [Grunt](http://gruntjs.com/)
+- [Jekyll](http://jekyllrb.com/)
+- [LiveReload](http://livereload.com/) (style injection, you'll need the [browser extensions](http://feedback.livereload.com/knowledgebase/articles/86242-how-do-i-install-and-use-the-browser-extensions-))
+
